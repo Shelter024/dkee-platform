e9879b4d6dc74df4c2e7735232178bf92d054a0f
shelter024 <gabadashelter97@gmail.com>
2025-11-27 19:15:02 +0000

    feat: add bundle analyzer and optimizePackageImports experimental flag

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withPWA = require('next-pwa')({
   disable: true, // Disabled for now
   runtimeCaching: [],
 });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 // Temporarily disable Sentry to fix server exit issue
 // const { withSentryConfig } = require('@sentry/nextjs');
@@ -15,7 +18,12 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  experimental: {
+    optimizePackageImports: [
+      'react-icons',
+    ],
+  },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
 // module.exports = withSentryConfig(withPWA(nextConfig), { silent: true });
